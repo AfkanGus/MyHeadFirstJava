b67c5f2a38f5a768e0257db1e39ac602d2751ff1
Taly <talyrus@gmail.com>
2018-03-22 16:39:18 +0300

    Sources

---
+++ com/taly/VerySimpleChatServer/VerySimpleChatServer.java
@@ -0,0 +1,85 @@
+package com.taly.VerySimpleChatServer;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+/**
+ * Created by Taly on 22.03.2018.
+ */
+public class VerySimpleChatServer {
+	ArrayList clientOutputStream;
+
+	public class ClientHandler implements Runnable{
+		BufferedReader reader;
+		Socket socket;
+
+		public ClientHandler(Socket clientSocket) {
+			try {
+				socket = clientSocket;
+				InputStreamReader isReader = new InputStreamReader(socket.getInputStream());
+				reader = new BufferedReader(isReader);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+
+		@Override
+		public void run() {
+			String message;
+			try {
+				while ((message = reader.readLine()) != null){
+					System.out.println("read " + message);
+					tellEveryone(message);
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+
+	} // закрываем вложенный класс
+
+	public static void main(String[] args) {
+		new VerySimpleChatServer().go();
+	}
+
+	public void go(){
+		clientOutputStream = new ArrayList();
+		try {
+			ServerSocket serverSock = new ServerSocket(4242);
+
+			while (true){
+				Socket clientSocket = serverSock.accept();
+				PrintWriter writer = new PrintWriter(clientSocket.getOutputStream());
+				clientOutputStream.add(writer);
+
+				Thread t = new Thread(new ClientHandler(clientSocket));
+				t.start();
+				System.out.println("got a connection");
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	} // закрываем go()
+
+	public void tellEveryone(String message){
+		Iterator it = clientOutputStream.iterator();
+		while (it.hasNext()){
+			try {
+				PrintWriter writer = (PrintWriter)it.next();
+				writer.println(message);
+				writer.flush();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		} // закрываем while
+	} // закрываем tellEveryone
+
+}//
