462f515c12cf734bdbf30812050c2f4dd5cffc19
Taly <talyrus@gmail.com>
2018-03-27 16:16:26 +0300

    Sources

---
+++ com/taly/Jukebox2/Jukebox2.java
@@ -0,0 +1,43 @@
+package com.taly.Jukebox2;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Collections;
+
+/**
+ * Created by Taly on 27.03.2018.
+ */
+public class Jukebox2 {
+	ArrayList<String> songList = new ArrayList<String>();
+
+	public static void main(String[] args) {
+		new Jukebox2().go();
+	}
+
+	public void go(){
+		getSong();
+		System.out.println(songList);
+		Collections.sort(songList);
+		System.out.println(songList);
+	}
+
+	private void getSong(){
+		try {
+			File file = new File("d:\\songs.txt");
+			BufferedReader reader = new BufferedReader(new FileReader(file));
+			String line = null;
+			while ((line = reader.readLine()) != null) {
+				addSong(line);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void addSong(String lineToParse){
+		String[] tokens = lineToParse.split("/");
+		songList.add(tokens[0]);
+	}
+}
