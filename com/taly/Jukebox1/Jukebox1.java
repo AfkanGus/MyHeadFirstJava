aee0f2b63a7f99a0f3f80f4e66631c5b8f04ada3
Taly <talyrus@gmail.com>
2018-03-27 15:30:52 +0300

    Sources

---
+++ com/taly/Jukebox1/Jukebox1.java
@@ -0,0 +1,40 @@
+package com.taly.Jukebox1;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+
+/**
+ * Created by Taly on 27.03.2018.
+ */
+public class Jukebox1 {
+	ArrayList<String> songList = new ArrayList<String>();
+
+	public static void main(String[] args) {
+		new Jukebox1().go();
+	}
+
+	public void go(){
+		getSong();
+		System.out.println(songList);
+	}
+
+	private void getSong(){
+		try {
+		File file = new File("d:\\songs.txt");
+		BufferedReader reader = new BufferedReader(new FileReader(file));
+		String line = null;
+		while ((line = reader.readLine()) != null) {
+				addSong(line);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void addSong(String lineToParse){
+		String[] tokens = lineToParse.split("/");
+		songList.add(tokens[0]);
+	}
+}
