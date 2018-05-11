bc45b2d8878e24c6dad10316a9182156c75d7279
Taly <talyrus@gmail.com>
2018-05-11 16:36:51 +0300

    Sources

---
+++ com/taly/Jukebox6/Jukebox6.java
@@ -0,0 +1,63 @@
+package com.taly.Jukebox6;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashSet;
+
+/**
+ * Created by Taly on 27.03.2018.
+ */
+public class Jukebox6 {
+	ArrayList<Song> songList = new ArrayList<Song>();
+
+	public static void main(String[] args) {
+		new Jukebox6().go();
+	}
+
+//	class ArtistCompare implements Comparator<Song> {
+//
+//		@Override
+//		public int compare(Song o1, Song o2) {
+//			return o1.getArtist().compareTo(o2.getArtist());
+//		}
+//	}
+
+	public void go(){
+		getSong();
+		System.out.println(songList);
+		Collections.sort(songList);
+		System.out.println(songList);
+
+//		ArtistCompare artistCompare = new ArtistCompare();
+//		Collections.sort(songList, artistCompare);
+//
+//		System.out.println(songList);
+		HashSet<Song> songSet = new HashSet<Song>();
+		songSet.addAll(songList);
+		System.out.println(songSet);
+	}
+
+	private void getSong(){
+		try {
+			File file = new File("d:\\songsListMore.txt");
+			BufferedReader reader = new BufferedReader(new FileReader(file));
+			String line = null;
+			while ((line = reader.readLine()) != null) {
+				addSong(line);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void addSong(String lineToParse){
+		String[] tokens = lineToParse.split("/");
+
+		Song nextSong = new Song(tokens[0], tokens[1], tokens[2], tokens[3]);
+		songList.add(nextSong);
+	}
+}
---
+++ com/taly/Jukebox6/Song.java
@@ -0,0 +1,55 @@
+package com.taly.Jukebox6;
+
+/**
+ * Created by Taly on 27.03.2018.
+ */
+class Song implements Comparable<Song> {
+	String title;
+	String artist;
+	String rating;
+	String bpm;
+
+	@Override
+	public boolean equals(Object aSong) {
+		Song s = (Song) aSong;
+		return getTitle().equals(s.getTitle());
+	}
+
+	@Override
+	public int hashCode() {
+		return title.hashCode();
+	}
+
+	@Override
+	public int compareTo(Song o) {
+		return title.compareTo(o.getTitle());
+	}
+
+	Song(String t, String a, String r, String b){
+		title = t;
+		artist = a;
+		rating = r;
+		bpm = b;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public String getArtist() {
+		return artist;
+	}
+
+	public String getRating() {
+		return rating;
+	}
+
+	public String getBpm() {
+		return bpm;
+	}
+
+	@Override
+	public String toString() {
+		return title;
+	}
+}
